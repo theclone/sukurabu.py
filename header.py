4325588558af1ac23a92d1435a78858673fa57e4
Manu Kumar <mrkumar9001@gmail.com>
2020-06-11 21:04:42 -0700

    fix platform check

--- header.py
+++ header.py
@@ -14,9 +14,9 @@ youtube_key = config['init']['youtube_api']
 client = discord.Client()
 logger = logging.getLogger('Discord')
 if not discord.opus.is_loaded():
-    if platform == 'linux':
-        discord.opus.load_opus('libopus.so.1')
-    elif platform == 'win32':
+    if sys.platform == 'linux':
+        discord.opus.load_opus('libopus.so')
+    elif sys.platform == 'win32':
         discord.opus.load_opus('opus')
     else:
         logger.error("unsupported platform")
